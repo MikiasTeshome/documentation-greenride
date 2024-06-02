dbf1b25c76a7c231a2e9b6aff955ed04ffbb2dfb
Mikias Teshome <teshomemikias3@gmail.com>
2024-06-02 23:54:39 +0300

    Endone

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-  logo: <span>Green RIDE</span>,
+  logo: (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <img src="/logoblack.png" alt="Green RIDE" style={{ width: '50px', height: 'auto' }} />
+      <span style={{ marginLeft: '8px' }}>Green RIDE</span>
+    </div>
+  ),
   project: {
     link: 'https://github.com/MikiasTeshome/documentation-greenride',
   },
@@ -19,10 +24,9 @@ const config: DocsThemeConfig = {
   i18n: [
     { locale: 'en-US', text: 'English' },
     { locale: 'or', text: 'Oromiffa' },
-    { locale: 'am', text: 'አማርኛaaa' },
-  
+    { locale: 'am', text: 'አማርኛ' },
     // { locale: 'ar-SA', text: 'العربية', direction: 'rtl' }
   ]
-}
+};
 
-export default config
+export default config;
